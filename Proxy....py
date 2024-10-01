408f9ab379effaf2e21815937dd779d63d7bb7a4
Иван Лоадерав <loaderaw121@gmail.com>
2024-10-02 01:20:06 +0300

    1
    proxy...

---
+++ Proxy....py
@@ -0,0 +1,12 @@
+import requests
+
+proxy = {
+    'http': 'http://username:password@proxyserver:port',
+    'https': 'https://username:password@proxyserver:port'
+}
+
+try:
+    response = requests.get('http://example.com', proxies=proxy)
+    print(response.text)  
+except requests.exceptions.RequestException as e:
+    print(f"Ошибка: {e}")
